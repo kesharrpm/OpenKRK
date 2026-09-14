0037eaa94ecad40b740b58b838017f6b0e64a480
kesharrpm <kesharrpm@gmail.com>
2026-09-14 13:33:14 +0800

    feat(system): expose visuals, SoundFont and font controls

--- src/main/preload.cjs
+++ src/main/preload.cjs
@@ -2,12 +2,16 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('openkrk', {
   chooseBgvFolder: () => ipcRenderer.invoke('bgv:choose-folder'),
+  getVisualCatalog: () => ipcRenderer.invoke('media:visual-catalog'),
   chooseLibraryFolder: () => ipcRenderer.invoke('library:choose-folder'),
   rescanLibrary: () => ipcRenderer.invoke('library:rescan'),
   getLibraryStatus: () => ipcRenderer.invoke('library:status'),
-  getVisuals: () => ipcRenderer.invoke('media:visuals'),
   findSongByCode: code => ipcRenderer.invoke('library:find-code', code),
   searchSongs: (query, limit) => ipcRenderer.invoke('library:search', query, limit),
+  chooseSoundBank: () => ipcRenderer.invoke('soundbank:choose'),
+  getSoundBankStatus: () => ipcRenderer.invoke('soundbank:status'),
+  chooseFontFile: () => ipcRenderer.invoke('font:choose'),
+  readBinaryFile: filePath => ipcRenderer.invoke('file:read-binary', filePath),
   onLibraryProgress: callback => {
     const handler = (_event, payload) => callback(payload);
     ipcRenderer.on('library:scan-progress', handler);
